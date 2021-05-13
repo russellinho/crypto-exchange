54810b28ec9c552e0a5be2e018a433739a7b6c5f
Russell Creswell <rucreswe@cisco.com>
2021-05-13 04:05:49 -0400

    Added DB

---
+++ src/main/java/com/russ/cryptoexchange/OracleInitializer.java
@@ -0,0 +1,41 @@
+package com.russ.cryptoexchange;
+
+import oracle.jdbc.pool.OracleDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+@ConfigurationProperties("oracle")
+public class OracleInitializer {
+    private String username;
+    private String password;
+    private String url;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Bean
+    DataSource dataSource() throws SQLException {
+        System.out.println("username: " + this.username + " | password: " + this.password + " | url: " + this.url);
+        OracleDataSource dataSource = new OracleDataSource();
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+        dataSource.setURL(url);
+        dataSource.setImplicitCachingEnabled(true);
+        dataSource.setFastConnectionFailoverEnabled(true);
+        return dataSource;
+    }
+}
